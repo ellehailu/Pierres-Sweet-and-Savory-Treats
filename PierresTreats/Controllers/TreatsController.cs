a01b2389a902ef3f7c64bdf89fc64de740c75317
ellehailu <hailu.elle@gmail.com>
2023-08-11 13:28:26 -0700

    Add migration to create db tables

--- PierresTreats/Controllers/TreatsController.cs
+++ PierresTreats/Controllers/TreatsController.cs
@@ -1,11 +1,36 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using PierresTreats.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace PierresTreats.Controllers
 {
     public class TreatsController : Controller
     {
+        private readonly PierresTreatsContext _dbContext;
 
+        public TreatsController(PierresTreatsContext db)
+        {
+            _db = db;
+        }
+        public ActionResult Index()
+        {
+            List<Treats> model = _db.Treats.ToList();
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Treat treat)
+        {
+            _db.Treats.Add(treat);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
