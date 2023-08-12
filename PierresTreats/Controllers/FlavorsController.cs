48efaf1f346c72e2fb178dcc6e2e27483062dc38
ellehailu <hailu.elle@gmail.com>
2023-08-11 18:21:31 -0700

    add create, details, and edit functionality to flavor

--- PierresTreats/Controllers/FlavorsController.cs
+++ PierresTreats/Controllers/FlavorsController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using PierresTreats.Models;
 
@@ -6,6 +7,52 @@ namespace PierresTreats.Controllers
 {
     public class FlavorsController : Controller
     {
+        private readonly PierresTreatsContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
 
+        public FlavorsController(UserManager<ApplicationUser> userManager, PierresTreatsContext db)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            List<Flavor> model = _db.Flavors.ToList();
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Flavor flavor)
+        {
+            _db.Flavors.Add(flavor);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            Flavor thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+            return View(thisFlavor);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Flavor thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+            return View(thisFlavor);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Flavor flavor)
+        {
+            _db.Flavors.Update(flavor);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
