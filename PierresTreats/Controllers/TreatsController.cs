8ffbeffffc3d6eb690914f3f200b970ff0791e07
ellehailu <hailu.elle@gmail.com>
2023-08-11 17:40:34 -0700

    Add edit and delete functionality to treat

--- PierresTreats/Controllers/TreatsController.cs
+++ PierresTreats/Controllers/TreatsController.cs
@@ -3,22 +3,26 @@ using Microsoft.AspNetCore.Mvc;
 using PierresTreats.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 namespace PierresTreats.Controllers
 {
+    [Authorize]
     public class TreatsController : Controller
     {
-        private readonly PierresTreatsContext _dbContext;
+        private readonly PierresTreatsContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        // public TreatsController(PierresTreatsContext db)
-        // {
-        //     _db = db;
-        // }
+        public TreatsController(UserManager<ApplicationUser> userManager, PierresTreatsContext db)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
         public ActionResult Index()
         {
-            // List<Treat> model = _db.Treats.ToList();
-            // return View(model);
-            return View();
+            List<Treat> model = _db.Treats.ToList();
+            return View(model);
         }
 
         public ActionResult Create()
@@ -29,8 +33,43 @@ namespace PierresTreats.Controllers
         [HttpPost]
         public ActionResult Create(Treat treat)
         {
-            // _db.Treats.Add(treat);
-            // _db.SaveChanges();
+            _db.Treats.Add(treat);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+            return View(thisTreat);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+            return View(thisTreat);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Treat treat)
+        {
+            _db.Treats.Update(treat);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Treat thisTreat = _db.Treats.FirstOrDefault(treats => treats.TreatId == id);
+            return View(thisTreat);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmation(int id)
+        {
+            Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+            _db.Treats.Remove(thisTreat);
+            _db.SaveChanges();
             return RedirectToAction("Index");
         }
     }
